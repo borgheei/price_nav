c2e2b584fa3f043acb1d35039fe215d7b43c0602
aliakbar <Hossein.asmand@yahoo.com>
2024-11-06 20:26:32 +0330

    upload project

---
+++ pricenav/pnav/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PnavConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pnav'
---
+++ pricenav/pricenav/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pricenav project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricenav.settings')
+
+application = get_asgi_application()
